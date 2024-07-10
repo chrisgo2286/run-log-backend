40321d6dc58fdfc688453385889f2489283d2a10
Christian Go <chrisgo2286@yahoo.com>
2024-07-10 02:05:53 -0400

    Worked on RunData

--- run/misc/run_data.py
+++ run/misc/run_data.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from calendar import monthrange
+
 class RunData:
     """Class to compile runs for given month and year"""
     def __init__(self, runs, month, year):
@@ -11,6 +14,8 @@ class RunData:
         self.filter_by_date()
         for run in self.runs:
             self.compile_single_run(run)
+        self.adjust_calendar_start()
+        self.adjust_calendar_end()
 
     def filter_by_date(self):
         """Filters queryset by month and year"""
@@ -22,7 +27,37 @@ class RunData:
         self.data.append({
             'run_type': run.run_type,
             'date': run.date,
+            'day': run.date.day,
             'distance': run.distance,
             'time': run.time,
             'comment': run.comment
         })
+
+    def adjust_calendar_start(self):
+        """Appends blank days prior to start of month for previous month"""
+        current_date = datetime.date(self.year, self.month, 1)
+        day_of_week = current_date.weekday()
+        
+        if day_of_week == 6:
+            return
+        
+        for day in range(day_of_week + 1):
+            self.data.append({'day': '0'})
+
+    def adjust_calendar_end(self):
+        """Appends blank days after end of month for next month"""
+        last_day = monthrange(self.year, self.month)[1]
+        last_date = datetime.date(self.year, self.month, last_day)
+        day_of_week = last_date.weekday()
+
+        if day_of_week == 5:
+            return
+        elif day_of_week == 6:
+            offset = 6
+        else:
+            offset = 5 - day_of_week
+
+        for day in range(offset):
+            self.data.append({'day': '0'})
+
+
