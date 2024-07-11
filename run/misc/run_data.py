76a3c499e70fd0b9dda3837041355ad34234c4bb
Christian Go <chrisgo2286@yahoo.com>
2024-07-10 23:38:15 -0400

    Worked on RunData

--- run/misc/run_data.py
+++ run/misc/run_data.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date
 from calendar import monthrange
 
 class RunData:
@@ -11,18 +11,23 @@ class RunData:
 
     def compile(self):
         """Compiles all run data"""
-        self.filter_by_date()
-        for run in self.runs:
-            self.compile_single_run(run)
+        self.runs = self.runs.filter(date__month=self.month, 
+            date__year=self.year)
+        self.add_calendar_days()
         self.adjust_calendar_start()
-        self.adjust_calendar_end()
+        self.adjust_calendar_end()        
 
-    def filter_by_date(self):
-        """Filters queryset by month and year"""
-        self.runs.filter(date__month=self.month)
-        self.runs.filter(date__year=self.year)
- 
-    def compile_single_run(self, run):
+    def add_calendar_days(self):
+        """Fills in days of the month and adds run if present"""
+        for day in range(1, monthrange(self.year, self.month)[1]):
+            run = self.runs.filter(date__year=self.year, date__month=self.month,
+                date__day=day)
+            if len(run) > 0:
+                self.add_single_run(run[0])
+            else:
+                self.data.append({'day':day})
+
+    def add_single_run(self, run):
         """Creates dict of single run and appends to self.data"""
         self.data.append({
             'run_type': run.run_type,
@@ -35,19 +40,19 @@ class RunData:
 
     def adjust_calendar_start(self):
         """Appends blank days prior to start of month for previous month"""
-        current_date = datetime.date(self.year, self.month, 1)
+        current_date = date(self.year, self.month, 1)
         day_of_week = current_date.weekday()
         
         if day_of_week == 6:
             return
         
         for day in range(day_of_week + 1):
-            self.data.append({'day': '0'})
+            self.data.insert(0, {'day': '0'})
 
     def adjust_calendar_end(self):
         """Appends blank days after end of month for next month"""
         last_day = monthrange(self.year, self.month)[1]
-        last_date = datetime.date(self.year, self.month, last_day)
+        last_date = date(self.year, self.month, last_day)
         day_of_week = last_date.weekday()
 
         if day_of_week == 5:
@@ -57,7 +62,7 @@ class RunData:
         else:
             offset = 5 - day_of_week
 
-        for day in range(offset):
+        for _ in range(offset):
             self.data.append({'day': '0'})
 
 
